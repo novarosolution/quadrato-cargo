05aecd5b718262cad3aa1f6dec43e1a1e47ff6f9
KULDIP <kuldip@KULDIPs-MacBook-Air.local>
2026-03-26 13:27:36 +0530

    tring to fix erorr

--- quadrato-cargo/next.config.ts
+++ quadrato-cargo/next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
-import path from "node:path";
-
-const appRoot = path.resolve(__dirname);
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  outputFileTracingRoot: appRoot,
-  turbopack: {
-    root: appRoot,
-  },
 };
 
 export default nextConfig;
